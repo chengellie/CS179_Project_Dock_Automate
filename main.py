c33e03e04edbc6ca1d7797d20cf76f5053a95f94
ECheng742 <echen111@ucr.edu>
2023-02-21 01:21:28 -0800

    Added implementation for initializing and checking goal state

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from ship import Ship
 from log import Log
 
-def create_ship(filename):
-    '''Input filename of manifest, parses file contents. Returns ship object.'''
+
+def create_ship(manifest_filename, op_filename):
+    """Input filename of manifest, parses file contents. Returns ship object."""
     # https://www.pythontutorial.net/python-basics/python-read-text-file/
-    with open(filename) as f:
-        contents = [line for line in f.readlines()]
-    ret = Ship(contents)
+    with open(manifest_filename) as f:
+        manifest_cntnt = [line for line in f.readlines()]
+    with open(op_filename) as f:
+        loads = f.readline().strip().split(",")
+        unloads = f.readline().strip().split(",")
+    ret = Ship(manifest_cntnt, loads, unloads)
+    print(ret.check_goal_state())
     return ret
 
-if __name__=='__main__':
-    create_ship('ShipCase1.txt')
-    Log('').writelog("Testing Log")
+
+if __name__ == "__main__":
+    create_ship("ShipCase1.txt", "load_unload.txt")
+    Log("").writelog("Testing Log")
--- ship.py
+++ ship.py
@@ -2,14 +2,17 @@ from container import Container
 
 
 class Ship:
-    def __init__(self, manifest):
+    def __init__(self, manifest, loads, unloads):
         self.ship_state = []
+        self.curr_state = {}
+        self.goal_state = {}
         self.row = 8
         self.col = 12
-        self.__init_ship(manifest)
+        self.__init_ship_state(manifest)
+        self.__init_goal_state(loads, unloads)
         print(self)
 
-    def __init_ship(self, manifest):
+    def __init_ship_state(self, manifest):
         """Input list of strings from manifest, constructs container objects and fills ship object with containers. Returns None."""
         self.ship_state = [[None for j in range(self.col)] for i in range(self.row)]
 
@@ -25,6 +28,48 @@ class Ship:
             for j in range(self.col):
                 self.ship_state[i][j] = containers[k]
                 k += 1
+
+    def __init_goal_state(self, loads, unloads):
+        """Inputs None, constructs map and fills dictionary with number of each type of container for starting state of ship,
+        constructs dictionary with goal state of number of each type of container. Returns None.
+        """
+        for row in self.ship_state:
+            for cntr_obj in row:
+                cntr_name = cntr_obj.name
+                if cntr_name == "UNUSED" or cntr_name == "NAN":
+                    continue
+                if cntr_name in self.goal_state.keys():
+                    self.goal_state[cntr_name] += 1
+                else:
+                    self.goal_state[cntr_name] = 1
+
+        # Construct goal state by simulating ship state after all actions completed
+        for cntr_name in loads:
+            if cntr_name in self.goal_state.keys():
+                self.goal_state[cntr_name] += 1
+            else:
+                self.goal_state[cntr_name] = 1
+
+        for cntr_name in unloads:
+            if cntr_name in self.goal_state.keys():
+                self.goal_state[cntr_name] -= 1
+                if self.goal_state[cntr_name] == 0:
+                    del self.goal_state[cntr_name]
+            else:
+                print("Error: trying to unload item that is not in ship")
+
+    def check_goal_state(self) -> bool:
+        """Inputs None, Returns if current ship_state matches goal_state"""
+        for row in self.ship_state:
+            for cntr_obj in row:
+                cntr_name = cntr_obj.name
+                if cntr_name == "UNUSED" or cntr_name == "NAN":
+                    continue
+                if cntr_name in self.curr_state.keys():
+                    self.curr_state[cntr_name] += 1
+                else:
+                    self.curr_state[cntr_name] = 1
+        return self.curr_state == self.goal_state
 
     def __str__(self) -> str:
         ret = ""
