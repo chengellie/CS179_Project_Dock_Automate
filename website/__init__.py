fdea18db7bf8629cac3affccff5ffafafe845275
faultylegend <sahaspoyekar@gmail.com>
2023-03-22 20:06:38 -0700

    Created Noted Taking Feature

--- website/__init__.py
+++ website/__init__.py
@@ -4,12 +4,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'apieuab920iwehfo'
 
-    from .views import home_page, tables, selection_1, unloading, loading
+    from .views import home_page, tables, selection_1, unloading, loading, notes
 
     app.register_blueprint(home_page, url_prefix = '/')
     app.register_blueprint(selection_1, url_prefix = '/home-selection')
     app.register_blueprint(tables, url_prefix = '/table')
     app.register_blueprint(unloading, url_prefix = '/unload')
     app.register_blueprint(loading, url_prefix = '/load')
+    app.register_blueprint(notes, url_prefix = '/notes')
 
     return app
